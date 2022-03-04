a0f612dffaa0ccefc664532d28b6800cc28c6329
Jessica Mulein <jessica@brightchain.org>
2022-03-04 11:51:33 -0800

    dotnet 6/nuget/refactor

--- src/CheckDigitExtension.cs
+++ src/CheckDigitExtension.cs
@@ -1,246 +1,265 @@
-﻿namespace BrightChain.Engine.Extensions
+﻿using System.Globalization;
+
+#nullable enable
+namespace BrightChain.Engine.Extensions;
+
+/// <summary>
+///     LUHN Modulo N / variable base for C#.
+///     Tested in base 10 and 16.
+///     Improved from https://stackoverflow.com/a/23640453.
+/// </summary>
+public static class CheckDigitExtension
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    private const int MinBase = 2;
+    private const int MaxBase = 16;
+
+    private static void CheckBase(int value)
+    {
+        if (value is < MinBase or > MaxBase)
+            throw new ArgumentOutOfRangeException(paramName: nameof(value),
+                message: $"Base must be between {MinBase} and {MaxBase}.");
+    }
+
+    private static int[] NewResultInitializationVectorBuffer(int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        var result = new int[digitsBase];
+        var offset = 0;
+        for (var i = 0; i < digitsBase; i += 2) result[offset++] = i;
+
+        for (var i = 1; i < digitsBase; i += 2) result[offset++] = i;
+
+        return result;
+    }
+
+    #region extension methods for IList<int>
 
     /// <summary>
-    /// LUHN Modulo N / variable base for C#.
-    /// Tested in base 10 and 16.
-    /// Improved from https://stackoverflow.com/a/23640453.
+    ///     For a list of digits, compute the ending checkdigit
     /// </summary>
-    public static class CheckDigitExtension
+    /// <param name="digits">The list of digits for which to compute the check digit</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the check digit</returns>
+    public static int CheckDigit(this IList<int> digits, int digitsBase = 10)
     {
-        const int MAX_BASE = 16;
-
-        static int[][] ResultsCacheByBase = new int[MAX_BASE + 1][];
-
-        static int[] NewResultInitializationVectorBuffer(int stringBase = 10)
-        {
-            var cachedResult = ResultsCacheByBase[stringBase];
-            if (cachedResult is not null)
-            {
-                return cachedResult;
-            }
-
-            int[] result = new int[stringBase];
-            int offset = 0;
-            for (int i = 0; i < stringBase; i += 2)
-            {
-                result[offset++] = i;
-            }
-
-            for (int i = 1; i < stringBase; i += 2)
-            {
-                result[offset++] = i;
-            }
-
-            ResultsCacheByBase[stringBase] = result;
-
-            return result;
-        }
-
-        #region extension methods for IList<int>
-
-        /// <summary>
-        /// For a list of digits, compute the ending checkdigit 
-        /// </summary>
-        /// <param name="digits">The list of digits for which to compute the check digit</param>
-        /// <returns>the check digit</returns>
-        public static int CheckDigit(this IList<int> digits, int stringBase = 10)
-        {
-            var results = NewResultInitializationVectorBuffer(stringBase: stringBase);
-            var i = 0;
-            var lengthMod = digits.Count % 2;
-            return (digits.Sum(d => i++ % 2 == lengthMod ? d : results[d]) * (stringBase - 1)) % stringBase;
-        }
-
-        /// <summary>
-        /// Return a list of digits including the checkdigit
-        /// </summary>
-        /// <param name="digits">The original list of digits</param>
-        /// <returns>the new list of digits including checkdigit</returns>
-        public static IList<int> AppendCheckDigit(this IList<int> digits, int stringBase = 10)
-        {
-            var result = new List<int>(digits);
-            result.Add(result.CheckDigit(stringBase: stringBase));
-            return result;
-        }
-
-        /// <summary>
-        /// Returns true when a list of digits has a valid checkdigit
-        /// </summary>
-        /// <param name="digits">The list of digits to check</param>
-        /// <returns>true/false depending on valid checkdigit</returns>
-        public static bool HasValidCheckDigit(this IList<int> digits, int stringBase = 10)
-        {
-            return digits.Last() == CheckDigit(
-                digits: digits.Take(digits.Count - 1).ToList(),
-                stringBase: stringBase);
-        }
-
-        #endregion extension methods for IList<int>
-
-        #region extension methods for strings
-
-        /// <summary>
-        /// Internal conversion function to convert string into a list of ints
-        /// </summary>
-        /// <param name="digits">the original string</param>
-        /// <returns>the list of ints</returns>
-        private static IList<int> ToDigitList(this string digits, int stringBase = 10)
-        {
-            return digits.Select(
-                d => Convert.ToInt32(
-                    value: d.ToString(),
-                    fromBase: stringBase))
-                        .ToList();
-        }
-
-        /// <summary>
-        /// For a string of digits, compute the ending checkdigit 
-        /// </summary>
-        /// <param name="digits">The string of digits for which to compute the check digit</param>
-        /// <returns>the check digit</returns>
-        public static string CheckDigit(this string digits, int stringBase = 10)
-        {
-            return Convert.ToString(
-                value: digits
-                    .ToDigitList(stringBase: stringBase)
-                    .CheckDigit(stringBase: stringBase),
-                toBase: stringBase);
-        }
-
-        /// <summary>
-        /// Return a string of digits including the checkdigit
-        /// </summary>
-        /// <param name="digits">The original string of digits</param>
-        /// <returns>the new string of digits including checkdigit</returns>
-        public static string AppendCheckDigit(this string digits, int stringBase = 10)
-        {
-            return digits + digits.CheckDigit(stringBase: stringBase);
-        }
-
-        /// <summary>
-        /// Returns true when a string of digits has a valid checkdigit
-        /// </summary>
-        /// <param name="digits">The string of digits to check</param>
-        /// <returns>true/false depending on valid checkdigit</returns>
-        public static bool HasValidCheckDigit(this string digits, int stringBase = 10)
-        {
-            return digits
-                .ToDigitList(stringBase: stringBase)
-                .HasValidCheckDigit(stringBase: stringBase);
-        }
-
-        #endregion extension methods for strings
-
-        #region extension methods for integers
-
-        /// <summary>
-        /// Internal conversion function to convert int into a list of ints, one for each digit
-        /// </summary>
-        /// <param name="digits">the original int</param>
-        /// <returns>the list of ints</returns>
-        private static IList<int> ToDigitList(this int digits, int stringBase = 10)
-        {
-            return Convert.ToString(
-                value: digits,
-                toBase: stringBase)
-                    .Select(
-                        d => Convert.ToInt32(
-                            value: d.ToString(),
-                            fromBase: stringBase)).ToList();
-        }
-
-        /// <summary>
-        /// For an integer, compute the ending checkdigit 
-        /// </summary>
-        /// <param name="digits">The integer for which to compute the check digit</param>
-        /// <returns>the check digit</returns>
-        public static int CheckDigit(this int digits, int stringBase = 10)
-        {
-            return digits
-                .ToDigitList(stringBase: stringBase)
-                .CheckDigit(stringBase: stringBase);
-        }
-
-        /// <summary>
-        /// Return an integer including the checkdigit
-        /// </summary>
-        /// <param name="digits">The original integer</param>
-        /// <returns>the new integer including checkdigit</returns>
-        public static int AppendCheckDigit(this int digits, int stringBase = 10)
-        {
-            return digits * stringBase + digits.CheckDigit(stringBase: stringBase);
-        }
-
-        /// <summary>
-        /// Returns true when an integer has a valid checkdigit
-        /// </summary>
-        /// <param name="digits">The integer to check</param>
-        /// <returns>true/false depending on valid checkdigit</returns>
-        public static bool HasValidCheckDigit(this int digits, int stringBase = 10)
-        {
-            return digits
-                .ToDigitList(stringBase: stringBase)
-                .HasValidCheckDigit(stringBase: stringBase);
-        }
-
-        #endregion extension methods for integers
-
-        #region extension methods for int64s
-
-        /// <summary>
-        /// Internal conversion function to convert int into a list of ints, one for each digit
-        /// </summary>
-        /// <param name="digits">the original int</param>
-        /// <returns>the list of ints</returns>
-        private static IList<int> ToDigitList(this Int64 digits, int stringBase = 10)
-        {
-            return Convert.ToString(
-                value: digits,
-                toBase: stringBase)
-                    .Select(
-                        d => Convert.ToInt32(
-                            value: d.ToString(),
-                            fromBase: stringBase)).ToList();
-        }
-
-        /// <summary>
-        /// For an integer, compute the ending checkdigit 
-        /// </summary>
-        /// <param name="digits">The integer for which to compute the check digit</param>
-        /// <returns>the check digit</returns>
-        public static int CheckDigit(this Int64 digits, int stringBase = 10)
-        {
-            return digits
-                .ToDigitList(stringBase: stringBase)
-                .CheckDigit(stringBase: stringBase);
-        }
-
-        /// <summary>
-        /// Return an integer including the checkdigit
-        /// </summary>
-        /// <param name="digits">The original integer</param>
-        /// <returns>the new integer including checkdigit</returns>
-        public static Int64 AppendCheckDigit(this Int64 digits, int stringBase = 10)
-        {
-            return digits * stringBase + digits.CheckDigit(stringBase: stringBase);
-        }
-
-        /// <summary>
-        /// Returns true when an integer has a valid checkdigit
-        /// </summary>
-        /// <param name="digits">The integer to check</param>
-        /// <returns>true/false depending on valid checkdigit</returns>
-        public static bool HasValidCheckDigit(this Int64 digits, int stringBase = 10)
-        {
-            return digits
-                .ToDigitList(stringBase: stringBase)
-                .HasValidCheckDigit(stringBase: stringBase);
-        }
-
-        #endregion extension methods for int64s
+        CheckBase(value: digitsBase);
+        var results = NewResultInitializationVectorBuffer(digitsBase: digitsBase);
+        var i = 0;
+        var lengthMod = digits.Count % 2;
+        return digits.Sum(selector: d => i++ % 2 == lengthMod ? d : results[d]) * (digitsBase - 1) % digitsBase;
     }
+
+    /// <summary>
+    ///     Return a list of digits including the check-digit
+    /// </summary>
+    /// <param name="digits">The original list of digits</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the new list of digits including check-digit</returns>
+    public static IList<int> AppendCheckDigit(this IList<int> digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        var result = new List<int>(collection: digits);
+        result.Add(item: result.CheckDigit(digitsBase: digitsBase));
+        return result;
+    }
+
+    /// <summary>
+    ///     Returns true when a list of digits has a valid check-digit
+    /// </summary>
+    /// <param name="digits">The list of digits to check</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>true/false depending on valid check-digit</returns>
+    public static bool HasValidCheckDigit(this IList<int> digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits.Last() == CheckDigit(
+            digits: digits.Take(count: digits.Count - 1).ToList(),
+            digitsBase: digitsBase);
+    }
+
+    #endregion extension methods for IList<int>
+
+    #region extension methods for strings
+
+    /// <summary>
+    ///     Internal conversion function to convert string into a list of ints
+    /// </summary>
+    /// <param name="digits">the original string</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the list of ints</returns>
+    private static IList<int> ToDigitList(this string digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits.Select(
+                selector: d => Convert.ToInt32(
+                    value: d.ToString(),
+                    fromBase: digitsBase))
+            .ToList();
+    }
+
+    /// <summary>
+    ///     For a string of digits, compute the ending check-digit
+    /// </summary>
+    /// <param name="digits">The string of digits for which to compute the check digit</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the check digit</returns>
+    public static string CheckDigit(this string digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return Convert.ToString(
+            value: digits
+                .ToDigitList(digitsBase: digitsBase)
+                .CheckDigit(digitsBase: digitsBase),
+            toBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Return a string of digits including the check-digit
+    /// </summary>
+    /// <param name="digits">The original string of digits</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the new string of digits including check-digit</returns>
+    public static string AppendCheckDigit(this string digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits + digits.CheckDigit(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Returns true when a string of digits has a valid check-digit
+    /// </summary>
+    /// <param name="digits">The string of digits to check</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>true/false depending on valid check-digit</returns>
+    public static bool HasValidCheckDigit(this string digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits
+            .ToDigitList(digitsBase: digitsBase)
+            .HasValidCheckDigit(digitsBase: digitsBase);
+    }
+
+    #endregion extension methods for strings
+
+    #region extension methods for integers
+
+    /// <summary>
+    ///     Internal conversion function to convert int into a list of ints, one for each digit
+    /// </summary>
+    /// <param name="digits">the original int</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the list of ints</returns>
+    private static IList<int> ToDigitList(this int digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits.ToString(
+                format: $"{digits.ToString()}",
+                provider: CultureInfo.InvariantCulture)
+            .ToDigitList(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     For an integer, compute the ending check-digit
+    /// </summary>
+    /// <param name="digits">The integer for which to compute the check digit</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the check digit</returns>
+    public static int CheckDigit(this int digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits
+            .ToDigitList(digitsBase: digitsBase)
+            .CheckDigit(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Return an integer including the check-digit
+    /// </summary>
+    /// <param name="digits">The original integer</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the new integer including check-digit</returns>
+    public static int AppendCheckDigit(this int digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits * digitsBase + digits.CheckDigit(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Returns true when an integer has a valid check-digit
+    /// </summary>
+    /// <param name="digits">The integer to check</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>true/false depending on valid check-digit</returns>
+    public static bool HasValidCheckDigit(this int digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits
+            .ToDigitList(digitsBase: digitsBase)
+            .HasValidCheckDigit(digitsBase: digitsBase);
+    }
+
+    #endregion extension methods for integers
+
+    #region extension methods for int64s
+
+    /// <summary>
+    ///     Internal conversion function to convert int into a list of ints, one for each digit
+    /// </summary>
+    /// <param name="digits">the original int</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the list of ints</returns>
+    private static IList<int> ToDigitList(this long digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        var resultDigits = new List<int>();
+        while (digits > 0)
+        {
+            resultDigits.Add(item: (int) (digits % digitsBase));
+            digits /= digitsBase;
+        }
+
+        return resultDigits;
+    }
+
+    /// <summary>
+    ///     For an integer, compute the ending check-digit
+    /// </summary>
+    /// <param name="digits">The integer for which to compute the check digit</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the check digit</returns>
+    public static int CheckDigit(this long digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits
+            .ToDigitList(digitsBase: digitsBase)
+            .CheckDigit(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Return an integer including the check-digit
+    /// </summary>
+    /// <param name="digits">The original integer</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>the new integer including check-digit</returns>
+    public static long AppendCheckDigit(this long digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits * digitsBase + digits.CheckDigit(digitsBase: digitsBase);
+    }
+
+    /// <summary>
+    ///     Returns true when an integer has a valid check-digit
+    /// </summary>
+    /// <param name="digits">The integer to check</param>
+    /// <param name="digitsBase"></param>
+    /// <returns>true/false depending on valid check-digit</returns>
+    public static bool HasValidCheckDigit(this long digits, int digitsBase = 10)
+    {
+        CheckBase(value: digitsBase);
+        return digits
+            .ToDigitList(digitsBase: digitsBase)
+            .HasValidCheckDigit(digitsBase: digitsBase);
+    }
+
+    #endregion extension methods for int64s
 }
--- test/CheckDigitExtensionTest.cs
+++ test/CheckDigitExtensionTest.cs
@@ -1,78 +1,114 @@
-﻿namespace BrightChain.Engine.Tests
+﻿using System;
+using System.Collections.Generic;
+using BrightChain.Engine.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrightChain.Engine.Tests;
+
+/// <summary>
+///     Improved from https://stackoverflow.com/a/23640453.
+/// </summary>
+[TestClass]
+public class CheckDigitExtensionTest
 {
-    using System;
-    using System.Collections.Generic;
-    using BrightChain.Engine.Extensions;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    /// <summary>
-    /// Improved from https://stackoverflow.com/a/23640453.
-    /// </summary>
-    [TestClass]
-    public class CheckDigitExtensionTest
+    [TestMethod]
+    public void ComputeCheckDigitsBase10()
     {
-        [TestMethod]
-        public void ComputeCheckDigitsBase10()
-        {
-            Assert.AreEqual(0, (new List<int> { 0 }).CheckDigit(stringBase: 10));
-            Assert.AreEqual(8, (new List<int> { 1 }).CheckDigit(stringBase: 10));
-            Assert.AreEqual(6, (new List<int> { 2 }).CheckDigit(stringBase: 10));
+        Assert.AreEqual(
+            expected: 0,
+            actual: new List<int> {0}.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 8,
+            actual: new List<int> {1}.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 6,
+            actual: new List<int> {2}.CheckDigit(digitsBase: 10));
 
-            Assert.AreEqual(0, (new List<int> { 3, 6, 1, 5, 5 }).CheckDigit(stringBase: 10));
-            Assert.AreEqual(0, 36155.CheckDigit(stringBase: 10));
-            Assert.AreEqual(8, (new List<int> { 3, 6, 1, 5, 6 }).CheckDigit(stringBase: 10));
-            Assert.AreEqual(8, 36156.CheckDigit(stringBase: 10));
-            Assert.AreEqual(6, 36157.CheckDigit(stringBase: 10));
-            Assert.AreEqual("6", "36157".CheckDigit(stringBase: 10));
-            Assert.AreEqual("3", "7992739871".CheckDigit(stringBase: 10));
-        }
+        Assert.AreEqual(
+            expected: 0,
+            actual: new List<int> {3, 6, 1, 5, 5}.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 0,
+            actual: 36155.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 8,
+            actual: new List<int> {3, 6, 1, 5, 6}.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 8,
+            actual: 36156.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: 6,
+            actual: 36157.CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: "6",
+            actual: "36157".CheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: "3",
+            actual: "7992739871".CheckDigit(digitsBase: 10));
+    }
 
-        [TestMethod]
-        public void ValidateCheckDigitsBase10()
-        {
-            Assert.IsTrue((new List<int> { 3, 6, 1, 5, 6, 8 }).HasValidCheckDigit(stringBase: 10));
-            Assert.IsTrue(361568.HasValidCheckDigit(stringBase: 10));
-            Assert.IsTrue("361568".HasValidCheckDigit(stringBase: 10));
-            Assert.IsTrue("79927398713".HasValidCheckDigit(stringBase: 10));
-        }
+    [TestMethod]
+    public void ValidateCheckDigitsBase10()
+    {
+        Assert.IsTrue(condition: new List<int> {3, 6, 1, 5, 6, 8}.HasValidCheckDigit(digitsBase: 10));
+        Assert.IsTrue(condition: 361568.HasValidCheckDigit(digitsBase: 10));
+        Assert.IsTrue(condition: "361568".HasValidCheckDigit(digitsBase: 10));
+        Assert.IsTrue(condition: "79927398713".HasValidCheckDigit(digitsBase: 10));
+    }
 
-        [TestMethod]
-        public void AppendCheckDigitsBase10()
-        {
-            Console.WriteLine("36156".CheckDigit(stringBase: 10));
-            Console.WriteLine("36156".AppendCheckDigit(stringBase: 10));
-            Assert.AreEqual("361568", "36156".AppendCheckDigit(stringBase: 10));
-            Assert.AreEqual("79927398713", "7992739871".AppendCheckDigit(stringBase: 10));
-        }
+    [TestMethod]
+    public void AppendCheckDigitsBase10()
+    {
+        Console.WriteLine(value: "36156".CheckDigit(digitsBase: 10));
+        Console.WriteLine(value: "36156".AppendCheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: "361568",
+            actual: "36156".AppendCheckDigit(digitsBase: 10));
+        Assert.AreEqual(
+            expected: "79927398713",
+            actual: "7992739871".AppendCheckDigit(digitsBase: 10));
+    }
 
-        [TestMethod]
-        public void ValidateCheckDigitsBase16()
-        {
-            Assert.IsTrue((new List<int> { 3, 10, 6, 13, 1, 15, 5, 6, 6 }).HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue(Convert.ToInt64(value: "3A6D1F566", fromBase: 16).HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("3A6D1F566".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("7A9D9F27398712".HasValidCheckDigit(stringBase: 16));
+    [TestMethod]
+    public void ValidateCheckDigitsBase16()
+    {
+        Assert.IsTrue(condition: new List<int> {3, 10, 6, 13, 1, 15, 5, 6, 6}.HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: Convert.ToInt64(
+            value: "3A6D1F566",
+            fromBase: 16).HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "3A6D1F566".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "7A9D9F27398712".HasValidCheckDigit(digitsBase: 16));
 
-            Assert.IsTrue("499602d2f".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("3A6D1F566".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("7A9D9F27398712".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("0B012722900021AC35B25".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("22111111111111111111f".HasValidCheckDigit(stringBase: 16));
-            Assert.IsTrue("211111111111111111111".HasValidCheckDigit(stringBase: 16));
-        }
+        Assert.IsTrue(condition: "499602d2f".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "3A6D1F566".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "7A9D9F27398712".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "0B012722900021AC35B25".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "22111111111111111111f".HasValidCheckDigit(digitsBase: 16));
+        Assert.IsTrue(condition: "211111111111111111111".HasValidCheckDigit(digitsBase: 16));
+    }
 
-        [TestMethod]
-        public void AppendCheckDigitsBase16()
-        {
-            Console.WriteLine("3A6D1F56".CheckDigit(stringBase: 16));
-            Console.WriteLine("3A6D1F56".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("499602d2f", "499602d2".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("3A6D1F566", "3A6D1F56".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("7A9D9F27398712", "7A9D9F2739871".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("0B012722900021AC35B25", "0B012722900021AC35B2".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("22111111111111111111f", "22111111111111111111".AppendCheckDigit(stringBase: 16));
-            Assert.AreEqual("211111111111111111111", "21111111111111111111".AppendCheckDigit(stringBase: 16));
-        }
-
+    [TestMethod]
+    public void AppendCheckDigitsBase16()
+    {
+        Console.WriteLine(value: "3A6D1F56".CheckDigit(digitsBase: 16));
+        Console.WriteLine(value: "3A6D1F56".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "499602d2f",
+            actual: "499602d2".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "3A6D1F566",
+            actual: "3A6D1F56".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "7A9D9F27398712",
+            actual: "7A9D9F2739871".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "0B012722900021AC35B25",
+            actual: "0B012722900021AC35B2".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "22111111111111111111f",
+            actual: "22111111111111111111".AppendCheckDigit(digitsBase: 16));
+        Assert.AreEqual(
+            expected: "211111111111111111111",
+            actual: "21111111111111111111".AppendCheckDigit(digitsBase: 16));
     }
 }
